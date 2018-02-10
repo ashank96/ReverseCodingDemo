d811bffb8dca84e72d005c2ac8bdfb23dc3c8487
ashank96 <bharatiashank1996@gmail.com>
2018-02-11 02:41:21 +0530

    all commits

---
+++ reverse_coding/Landing/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>DeCoders - Reverse Coding</title>
+	<link rel="icon" href="../commonResources/img/title.png">
+    <?php include('../commonResources/includeCss.php');?>
+
+</head>
+<body>
+	<?php include('../../commonResources/navigation.inc.php');?>
+	<!--poster-->
+							<div class="text-center">
+									<img src="../registration/reversecoding.png" hspace="105" width="1500" class="shift img-fluid" alt="">
+							</div>
+	<!--/.poster-->
+	<!--main content-->
+							<div class="main-content">
+								<div class="container">
+									<div class="header-title">
+										<h2 class="h2-responsive">Reverse Coding<a href="https://www.facebook.com/Sit.Decoders" target="_blank"><img src="../commonResources/img/fb.png" class="fb-icon"></a></h2>
+
+									</div>
+									<div class="row">
+									<div class="col-md-6">
+										<h4 class="h4-responsive question" style="font-family:Arial;">What is Reverse Coding?</h4>
+											<p style="line-height:130% ;font-size:1.1rem;">Reverse Coding is a brainstorming practice, a bit aloof from the regular coding, designed to have a lookup of one's coding skills by cracking down code based on the given outputs.
+                      </p>
+                        <p style="line-height:130% ;font-size:1.1rem;"> This would be a competition of 2 rounds. The participaints will be tested on the basis of their aptitude in the programming languages like C, as well as on their art of coding.
+											</p>
+                      <p style="line-height:130% ;font-size:1.1rem;"> It's your turn now to accept the challenge and take part in the event in order to compete with others and have a spotlight, by showcasing your coding skills.
+                    </p>
+
+									</div>
+									<div class="col-md-6">
+										<h4 class="h4-responsive question" style="font-family:Arial;">Prizes</h4>
+											    <!---<img src="../commonResources/img/1st.png" style="width:30%">
+												<img src="../commonResources/img/2nd.png" style="width:30%">
+												<img src="../commonResources/img/3rd.png" style="width:30%">!--->
+										<p style="line-height:130% ;font-size:1.1rem; color:green">*Participation certificate to each participant.</p>
+
+									</div>
+									</div>
+
+										<h4 class="h4-responsive question" style="font-family:Arial;">General Information</h4>
+												<table class="table table-striped" style="font-size:1.1rem;">
+													<tr>
+														<td>Date</td><td>17th February, 2018</td>
+													</tr>
+
+													<tr>
+														<td>Reporting time</td><td>02:15 PM</td>
+													</tr>
+													<tr>
+														<td>Round 1 starts at</td><td>02:30 PM</td>
+													</tr>
+													<tr>
+														<td>Round 1 ends at</td><td>03:15 PM</td>
+													</tr>
+                                                        <tr>
+														<td>Round 1 result announcement</td><td>04:00 PM</td>
+													   </tr>
+                                                    
+                                                    <tr>
+														<td>Round 2 starts at</td><td>04:30 PM</td>
+													</tr>
+													<tr>
+														<td>Round 2 ends at</td><td>06:00 PM</td>
+													</tr>
+													<tr>
+														<td>Venue</td><td>CS BLOCK, Ground Floor Labs, SIT TUMKUR</td>
+													</tr>
+													<tr>
+														<td>Participants</td><td>In a team of 2.</td>
+													</tr>
+													<tr>
+														<td>Registration Fee </td><td>Free of Cost, passion to code is the fuel !!</td>
+													</tr>
+													<tr>
+														<td>Registration end date</td><td>16th February, 2018(11:59PM)</td>
+													</tr>
+												</table>
+										<h4 class="h4-responsive question" style="font-family:Arial;">Event & registration details</h4>
+											<ol style="font-size:1.1rem;">
+												<li style="list-style-type: circle">Registrations has to be done in a team of 2.</li>
+												<li style="list-style-type: circle">Each team should be headed by a team leader.</li>
+												<li style="list-style-type: circle">The event would consist of 2 rounds.</li>
+                          <li style="list-style-type: circle">The only teams qualifying in the 1st round(by crossing the cut-off) would be selected for the second round.
+  													</li>
+												<li style="list-style-type: circle">You can register from the link below.</li>
+                        <li style="list-style-type: circle">We encourage students to register as soon as possible before the slots get full.</li>
+											</ol>
+										<br><br>
+										<center>
+										<button type="button" class="btn btn-primary" onclick="location.href='../registration/';"> Register Now</button>
+                  <!--  <button type="button" class="btn btn-primary" onclick="location.href='indexTemp.php';">View problem statements and register</button> -->
+										</center>
+
+
+								</div>
+							</div>
+			<!--main content-->
+	<?php include('../../commonResources/footer.inc.php');?>
+
+
+	 <!-- SCRIPTS -->
+
+    <!-- JQuery -->
+    <?php include('../commonResources/includeScripts.php');?>
+</body>
+</html>
---
+++ reverse_coding/registration/index.php
@@ -0,0 +1,207 @@
+<?php
+include 'testInput.php';
+$user='root';
+$pass='root';
+$db= 'reverse_coding';
+$error="";
+$errorFound=false;
+$teamErr=$lNameErr=$lUsnErr=$lEmailErr=$lPhoneErr=$mNameErr=$mUsnErr=$mEmailErr=$mPhoneErr="";
+if(array_key_exists("submit",$_POST)){
+
+    $link=mysqli_connect('localhost',$user,$pass,$db) or die('Error');
+    $query="Select * from `team` where teamName='".$_POST['teamName']."'";
+
+    $result=mysqli_query($link,$query);
+    $result2=mysqli_query($link,"Select * from `participants`");
+    if($_POST['leaderUsn']==$_POST['member1Usn']){
+        $mUsnErr.="*Leader USN and Member USN cannot be same";
+        $lUsnErr.="*Leader USN and Member USN cannot be same";
+        $errorFound=true;
+    }
+    if(mysqli_num_rows($result2)>0){
+        $i=0;
+        while($row=mysqli_fetch_array($result2)){
+            if($row['usn']==$_POST['leaderUsn']){
+                $lUsnErr.="*USN already Registered";
+                $i=-1;
+            }
+            if($row['usn']==$_POST['member1Usn']){
+                $mUsnErr.="*USN already Registered";
+                $i=-1;
+            }
+            if($i==-1)
+                $errorFound=true;
+          }
+        
+    }
+    if(mysqli_num_rows($result)>0){
+        $teamErr="*Team name already exists";   
+        $errorFound=true;
+    }
+    if(test_input($teamErr,$lNameErr,$lUsnErr,$lEmailErr,$lPhoneErr,$mNameErr,$mUsnErr,$mEmailErr,$mPhoneErr)==-1){
+        $error="*Some fields are still empty or have invalid entries";
+        $errorFound=true;
+    }
+    if(!$errorFound){
+        $queryTeam="Insert into `team` (`teamName`) values('".$_POST['teamName']."')";
+        mysqli_query($link,$queryTeam);
+        $query="Select * from `team` where teamName='".$_POST['teamName']."'";
+
+
+        $result=mysqli_query($link,$query);
+        $row=mysqli_fetch_array($result);
+        $queryParticipant1="Insert into `participants` (`name`,`usn`,`email`,`phone`,`teamId`) values('".$_POST['leaderName']."','".$_POST['leaderUsn']."','".$_POST['leaderEmail']."','".$_POST['leaderPhone']."',".$row['teamID'].")";
+
+
+        mysqli_query($link,$queryParticipant1);
+        $queryParticipant2="Insert into `participants` (`name`,`usn`,`email`,`phone`,`teamId`) values('".$_POST['member1Name']."','".$_POST['member1Usn']."','".$_POST['member1Email']."','".$_POST['member1Phone']."',".$row['teamID'].")";
+        mysqli_query($link,$queryParticipant2);
+        header("Location: success2.php");
+        
+
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>DeCoders - Reverse Coding</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <link rel="stylesheet" href="myOwn.css">
+
+    <?php include('../commonResources/includeCss.php');?>
+
+</head>
+<body>
+<?php include('../../commonResources/navigation.inc.php');?>
+<!--poster-->
+
+<!--/.poster-->
+<!--main content-->
+<div class=" view overlay hm-green-slight hm-zoom hoverable">
+    <img src="reversecoding.png" width=1200 height=100 class="shift img-fluid img-responsive  center-block"  alt="">
+    <a href="#">
+        <div class="mask waves-effect waves-light"></div>
+    </a>
+</div>
+<div class="main-content">
+    <form class="form-group1" method="post" >
+
+        <fieldset1 class="form-group1 row">
+            <div  class="jumbotron1 bg-dark1 text-white" style="font-family:serif;margin-top: -3.5%">
+                <div class="header-title">
+                    <h1 class="h2-responsive font-weight-bold " style="text-align: center;color: white;">Reverse Coding</a></h1>
+                    <span class="error"><?php echo $error ?></span>
+                </div>
+                <div style="margin-left: 15%;margin-right:15%;">
+
+                    <fieldset1 class="form-group1 form-row1">
+                        <label style="color: white" class="col-sm1-4"><h4><b>Team Name</b></h4></label>
+                        <div class="col-sm1-5">
+                            <input placeholder="team name" style="font-family:monospace; font-size: 120%; color:white;" type="text" class="form-control1 bg-dark1" id="teamName" name="teamName" >
+                            <span class="error"><?php echo $teamErr ?></span>
+                        </div>
+
+                    </fieldset1>
+                    <fieldset1 class="form-group1 form-row1">
+                        <label style="color: white" class="col-sm1-4"><h5><strong>Team Leader</strong></h5></label>
+                    </fieldset1>
+                    <fieldset1 class="form-group1 row header-title" style="margin-left: 3%">
+
+                        <fieldset1 class="form-group1 ">
+                            <label style="color: white" class="col-sm1-2">Name</label>
+                            <div class="col-sm1-5">
+                                <input style="font-family:monospace; color:white;" placeholder="full name" type="text" class="form-control1 bg-dark1" id="leaderName" name="leaderName"><span class="error"><?php echo $lNameErr ?></span>
+                            </div>
+
+                        </fieldset1>
+                        <fieldset1 class="form-group1 ">
+                            <label style="color: white" class="col-sm1-2">USN</label>
+                            <div class="col-sm1-5">
+                                <input style="font-family:monospace;color:white; text-transform: uppercase;" placeholder="1SIYYCSXYZ" type="text" class="form-control1 bg-dark1" id="text" name="leaderUsn"><span class="error"><?php echo $lUsnErr ?></span>
+                            </div>
+
+                        </fieldset1>
+                        <fieldset1 class="form-group1 ">
+                            <label style="color: white" class="col-sm1-2">E-mail</label>
+                            <div class="col-sm1-5">
+                                <input style="font-family:monospace;color: white;" placeholder="example@email.com" type="email" class="form-control1 bg-dark1" id="email" name="leaderEmail"><span class="error"><?php echo $lEmailErr ?></span>
+                            </div>
+
+                        </fieldset1>
+                        <fieldset1 class="form-group1 ">
+                            <label style="color: white" class="col-sm1-2">Phone</label>
+                            <div class="col-sm1-5">
+                                <input style="font-family:monospace;color: white;" placeholder="phone number" type="tel"  class="form-control1  bg-dark1" id="phone" name="leaderPhone"  maxlength="10"><span class="error"><?php echo $lPhoneErr ?></span>
+                            </div>
+
+                        </fieldset1>
+
+                    </fieldset1>
+
+                    <fieldset1 class="form-group1 form-row1">
+                        <label style="color: white" class="col-sm1-4"><h5><strong>Team Member 1</strong></h5></label>
+                    </fieldset1>
+                    <fieldset1 class="form-group1 row header-title" style="margin-left: 3%">
+
+
+                        <fieldset1 class="form-group1">
+                            <label style="color: white" class="col-sm1-2">Name</label>
+                            <div class="col-sm1-5">
+                                <input style="font-family:monospace;color: white;" placeholder="full name" type="text" class="form-control1 bg-dark1" id="leaderName" name="member1Name">
+                                <span class="error"><?php echo $mNameErr ?></span>
+                            </div>
+
+                        </fieldset1>
+                        <fieldset1 class="form-group1">
+                            <label style="color: white" class="col-sm1-2">USN</label>
+                            <div class="col-sm1-5">
+                                <input style="font-family:monospace;color: white; text-transform: uppercase;" placeholder="1SIYYCSXYZ" type="text" class="form-control1  bg-dark1" id="text" name="member1Usn">
+                                <span class="error"><?php echo $mUsnErr ?></span>
+                            </div>
+
+                        </fieldset1>
+                        <fieldset1 class="form-group1">
+                            <label style="color: white" class="col-sm1-2">E-mail</label>
+                            <div class="col-sm1-5">
+                                <input style="font-family:monospace;color: white;" placeholder="example@email.com" type="email" class="form-control1 bg-dark1" id="email" name="member1Email">
+                                <span class="error"><?php echo $mEmailErr ?></span>
+                            </div>
+
+                            <fieldset1 class="form-group1">
+                            <label style="color: white" class="col-sm1-2">Phone</label>
+                            <div class="col-sm1-5">
+                                <input style="font-family:monospace;color: white;"  placeholder="phone number" type="tel" class="form-control1 bg-dark1"   id="phone" name="member1Phone"  maxlength="10" >
+                                <span class="error"><?php echo $mPhoneErr ?></span>
+                            </div>
+
+                        </fieldset1><br><br>
+                    </fieldset1>
+                    <fieldset1 class="form-group1">
+                        <input style="float: right;margin-right: 20px;" type="submit" value="Register" name="submit" class="btn1  btn1-outline1-success1 font-weight-bold">
+                    </fieldset1>
+                </div>
+            </div>
+        </fieldset1>
+    </form>
+    <!--main content-->
+</div>
+    <?php include('../../commonResources/footer.inc.php');?>
+
+    <!-- SCRIPTS -->
+
+    <!-- JQuery -->
+    <?php include('../commonResources/includeScripts.php');?>
+</body>
+</html>
